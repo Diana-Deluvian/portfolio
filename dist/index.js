457066c136cb468ae65c214194c049ba2795bcd5
Diana Deluvian <diana.deluvian@gmail.com>
2021-10-27 17:21:47 +0200

    added single page functionality

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,39 @@
-document.getElementById("work-a").addEventListener("touchstart", function() {}, true);
+document.getElementById("work-projects").addEventListener("touchstart", function() {}, true);
+
+//document.querySelectorAll('[id*="work"]').forEach(element => element.style.display = "none");
+
+let sections = document.querySelectorAll('section');
+let header = document.getElementsByTagName("header")[0];
+
+let navigation = document.querySelectorAll("[class=navigation]");
+navigation.forEach(element => element.onclick = e => {
+    toggleCurrentClass(e);
+    let selector =  e.target.innerHTML.toLowerCase();
+    navigate(selector, e);
+});
+
+let toWorkButton = document.getElementById("to-projects");
+toWorkButton.onclick = (e) => {toggleCurrentClass();
+    navigate("work", e)}
+
+function navigate(selector, e) {
+    sections.forEach(element => element.style.display = "none");
+    document.querySelectorAll(`[id*=${selector}]`).forEach(element => element.style.display = "block");
+    if(selector === 'home') {
+        header.classList.remove("inner");
+    }
+    else {
+        
+        header.classList.add("inner");
+    }
+}
+
+function toggleCurrentClass(e) {
+    navigation.forEach(element => element.classList.remove("current"));
+    if(e) {e.target.classList.add("current");}
+    else {
+        navigation[2].classList.add("current");
+    }
+    
+}
+
