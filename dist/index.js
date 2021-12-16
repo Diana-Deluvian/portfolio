3c7f2f8d4eef42e2bb8cbc74817132eeeba9b28b
Diana Deluvian <diana.deluvian@gmail.com>
2021-12-16 19:18:43 +0100

    finalized form validation

--- dist/index.js
+++ dist/index.js
@@ -50,23 +50,54 @@ window.onload = () => {
     navigate("home");
 }
 
+const elem = document.getElementById('content');
+const contentRequiredStar = document.querySelectorAll('.form-required')[1];
+function hasScrollbar(){  
+    if (elem.clientHeight < elem.scrollHeight) contentRequiredStar.style.right = "1rem";
+    else contentRequiredStar.style.right = "0.4rem";
+}
 
 const formSubmitButton = document.getElementById("form-submit-button");
 const formStatus = document.querySelector('#form-status');
+const email = document.querySelector("#email");
+const content = document.querySelector("#content");
+
+function validateEmail(email) {
+    const regExpr = /\S+@\S+\.\S+/;
+    //checks for the patter *@*.*
+    return regExpr.test(email);
+}
+
 formSubmitButton.onclick = async e => {
     e.preventDefault();
+    formStatus.textContent = null;
+    formStatus.classList.remove("form-status-error");
+    
+
+    if(email.value == "" || content.value == ""){
+        formStatus.textContent = "Please fill in the required fields!";
+        formStatus.classList.add("form-status-error");
+        return false;
+    }
+
+    if(!validateEmail(email.value)) {
+        formStatus.textContent = "Please provide a valid e-mail address!";
+        formStatus.classList.add("form-status-error");
+        return false;
+    }
+
     formSubmitButton.textContent = "Submitting...";
     formSubmitButton.disabled = true;
     const data = Array.from(document.getElementsByTagName('input')).reduce((acc, input) => ({
         ...acc, [input.id]: input.value}), {});
     data.message = document.getElementsByTagName('textarea')[0].value;
     try {
-    const res = await fetch('https://dianas-portfolio-api.herokuapp.com/sendMail', {
+    const res = await fetch('http://localhost:8080/sendMail', {
       method: 'POST',
       headers: {'Content-Type':'application/json'},
       body: JSON.stringify(data),
     });
-    
+    console.log(res);
     if(res.status === 200) {
         document.querySelector('form').reset();
         formStatus.textContent = "Message sent. I will get back to you as soon as possible."
@@ -75,7 +106,7 @@ formSubmitButton.onclick = async e => {
     formSubmitButton.disabled = false;
     }
     else {
-        formStatus.textContent = "You done messed up ey-ey-run";
+        formStatus.textContent = "There was a problem sending your message. Please double check your input and try again!";
         formStatus.classList.add("form-status-error");
         formSubmitButton.textContent = "Submit";
     formSubmitButton.disabled = false;
@@ -89,3 +120,5 @@ catch {
 }
 
   }
+
+
